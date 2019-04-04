baa22d77b6cb4c80c9141dcd8d4d92d4b2eb3628
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-04-04 22:39:01 +0500

    Add NotImplementedException

--- dexbot/exceptions.py
+++ dexbot/exceptions.py
@@ -1,2 +1,8 @@
 class MissingSettingsException(Exception):
     pass
+
+
+class NotImplementedException(Exception):
+    """ This exception is an indicator that some feature is not implemented
+    """
+    pass
