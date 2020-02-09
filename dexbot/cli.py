dd82664db7a9c60c937b5be291c105a9a958a012
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-02-09 16:03:46 +0500

    Add small sleep when dropping state from cli
    Database worker is threaded and don't have a chance to exec a noreturn
    query, so small time is needed.

--- dexbot/cli.py
+++ dexbot/cli.py
@@ -4,6 +4,7 @@ import os
 import os.path
 import signal
 import sys
+import time
 from multiprocessing import freeze_support
 
 import click  # noqa: E402
@@ -145,6 +146,7 @@ def drop_state(worker_name):
     """
     click.echo('Dropping state for {}'.format(worker_name))
     Storage.clear_worker_data(worker_name)
+    time.sleep(1)
 
 
 def worker_job(worker, job):
